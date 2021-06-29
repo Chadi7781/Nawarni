109a3cd11d5c7ce4d670e3eda5e383644b3afba1
Shady7781 <troudishedy6@gmail.com>
2021-06-30 01:43:32 +0200

    design log in code

--- sign.php
+++ sign.php
@@ -1,4 +1,3 @@
-
 <?php
     require 'connect/DB.php';
     require 'core/load.php';
@@ -42,9 +41,8 @@
             else {
                 $userLink = $screenName;    
             }
-
             if(!preg_match("^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9]+)*(\.[a-z]{2,3})$^",$emailMobile)) {
-                if(!preg_match("^[0-12]{09}^",$emailMobile)) {
+                if(!preg_match("^[0-9]{8}^", $emailMobile)){
                     $error = "Email id or Mobile number is not correct. Please tyr again";
                 }
                 else {
@@ -62,7 +60,7 @@
                             $error = "Mobile is already exist.";
                         }
                         else {
-                            $loadFromUser->create('users', array('first_name'=>$firstName,'last_name'=>$lastName,
+                            $user_id = $loadFromUser->create('users', array('first_name'=>$firstName,'last_name'=>$lastName,
                             'mobile'=>$emailMobile,'password'=>$passwordHash,
                             "screenName"=>$screenName,"userLink"=>$userLink, 'birthday'=>$birth,
                             "gender"=>$upGen));
@@ -105,7 +103,6 @@
                 $loadFromUser->create('token', array('token'=>$token,'user_id'=>$user_id));
 
                setcookie('FBID', $token, time() + 60 *60*24*7, '/', NULL, NULL, true );
-
                //Redirect the user to index page
                header('Location: index.php');
 
@@ -132,6 +129,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -139,73 +137,104 @@
     <title>Nawarni</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
-    <div class="header"></div>
-        <div class="main">
-            <div class="left-side">
-                <img src="assets/images/signPicture.png" alt="share_sign">
-            </div>
-            <div class="right-side">
-                <div class="error">
-                <?php if(!empty($error))  echo $error;?>
+    <div class="header">
+        <div class="logo">Nawarni</div>
+        <form action="sign.php" method="post">
+            <div class="sign-in-form">
+                <div class="mobile-input">
+                    <div class="input-text">
+                        Email or Phone
+                    </div>
+                    <input type="text" name="in-email-mobile" id="email-mobile" class="input-text-field">
                 </div>
-                    <h1 style="color:#212121;">Create an account</h1>
-                    <div style="color:#212121;font-size:20px;" >it's free and always will be</div>
-                    <!--Form start -->
-                    <form action="sign.php" method="POST" name="user-sign-up">
-                        <div class="sign-up-form">
-                            <div class="sign-up-name">
-                                <input type="text" name="first-name" id="first-name" class="text-field" placeholder="First Name">
-                                <input type="text" name="last-name" id="last-name" class="text-field" placeholder="Last Name">
-                            </div>
-                            <div class="sign-wrap-mobile">
-                                <input type="text" name="email-mobile" id="email-mobile" class="text-input" placeholder="Mobile number or email address" class="text-input">
-                            </div>
-                            <div class="sign-up-password">
-                                <input type="password" name="up-password" id="up-password" class="text-input">
-                            </div>
-                            <div class="sign-up-birthday">
-                                <div class="bday">Birthday</div>
-                                <div class="form-birthday">
-                                    <select name="birth-day" id="days" class="select-body">
-                                    </select>
-                                    <select name="birth-month" id="months" class="select-body">
-                                    </select>
-                                    <select name="birth-year" id="years" class="select-body">
-                                    </select>
-                                </div>
-                            </div>
+                <div class="passowrd-input">
+                    <div class="input-text-password">Password</div>
 
-                            <div class="gender-wrap">
-                                <input type="radio" name="gen" id="fem" value="female" class="m0">
-                                <label for="fem" class="gender">Female</label>
-                                <input type="radio" name="gen" id="male" value="male" class="m0">
-                                <label for="male" class="gender">Male</label>
-                           
-                            </div>
-                            <div class="term">
-                                By clicking Sign Up, you agree to our terms, Data policy and Cookie policy. You may receive SMS notifications from us and can opt out at any time.
+                    <input type="password" name="in-pass" id="in-password" class="input-text-field">
 
-                            </div>
-                            <button type="submit" value="Sign Up" class="sign-up-button">Sign Up
-                        </div>
-                    
-                    </form>
+                    <div class="forgotten-acc">Forgotten account</div>
 
-                    <!--Form end -->
+                </div>
+
+                <div class="login-button">
+                    <input type="submit" value="Log in" class="sign-in login">
+                </div>
+
 
             </div>
+        </form>
+    </div>
+    <div class="main">
+        <div class="left-side">
+            <img src="assets/images/signPicture.png" alt="share_sign">
+        </div>
+        <div class="right-side">
+            <div class="error">
+                <?php if(!empty($error))  echo $error;?>
+            </div>
+            <h1 style="color:#212121;">Create an account</h1>
+            <div style="color:#212121;font-size:20px;">it's free and always will be</div>
+            <!--Form start -->
+            <form action="sign.php" method="POST" name="user-sign-up">
+                <div class="sign-up-form">
+                    <div class="sign-up-name">
+                        <input type="text" name="first-name" id="first-name" class="text-field"
+                            placeholder="First Name">
+                        <input type="text" name="last-name" id="last-name" class="text-field" placeholder="Last Name">
+                    </div>
+                    <div class="sign-wrap-mobile">
+                        <input type="text" name="email-mobile" id="email-mobile" class="text-input"
+                            placeholder="Mobile number or email address" class="text-input">
+                    </div>
+                    <div class="sign-up-password">
+                        <input type="password" name="up-password" id="up-password" class="text-input">
+                    </div>
+                    <div class="sign-up-birthday">
+                        <div class="bday">Birthday</div>
+                        <div class="form-birthday">
+                            <select name="birth-day" id="days" class="select-body">
+                            </select>
+                            <select name="birth-month" id="months" class="select-body">
+                            </select>
+                            <select name="birth-year" id="years" class="select-body">
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="gender-wrap">
+                        <input type="radio" name="gen" id="fem" value="female" class="m0">
+                        <label for="fem" class="gender">Female</label>
+                        <input type="radio" name="gen" id="male" value="male" class="m0">
+                        <label for="male" class="gender">Male</label>
+
+                    </div>
+                    <div class="term">
+                        By clicking Sign Up, you agree to our terms, Data policy and Cookie policy. You may receive SMS
+                        notifications from us and can opt out at any time.
+
+                    </div>
+                    <button type="submit" value="Sign Up" class="sign-up-button">Sign Up
+                </div>
+
+            </form>
+
+            <!--Form end -->
 
         </div>
-        
-        <!--Jquery library-->
-        <script src="assets/js/jquery.min.js"></script>
 
-        <!--Main Js-->
-        <script src="assets/js/main.js"></script>
+    </div>
+
+    <!--Jquery library-->
+    <script src="assets/js/jquery.min.js"></script>
+
+    <!--Main Js-->
+    <script src="assets/js/main.js"></script>
 
 
 
 
 </body>
+
 </html>
