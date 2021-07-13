f6e51a08fdb2a98d162418055bda44d88d12872d
Shady7781 <troudishedy6@gmail.com>
2021-07-13 13:19:45 +0200

    init profile html code

---
+++ profile.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Chadi Troudi</title>
+</head>
+
+<body>
+    <header>
+    </header>
+    <main>
+        <div class="main-area">
+            <div class="profile-left-wrap">
+                <div class="profile-cover-wrap"> </div>
+                <div class="cover-bottom-part"></div>
+                <div class="bio-timeline">
+                    <div class="bio-wrap"></div>
+                    <div class="status-timeline-wrap"></div>
+                </div>
+            </div>
+            <div class="profile-right-wrap"> </div>
+        </div>
+        <div class="top-box-show"></div>
+        <div id="adv_dem"> </div>
+    </main>
+</body>
+
+</html>
